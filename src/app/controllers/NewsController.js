c97c9d15140c84fe80ffd105bb4a2b4c39523e25
Le Chau Kiet <lck.dev7014@gmail.com>
2025-09-14 21:10:39 +0700

    add project

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,15 @@
+
+class NewsController {
+
+    //[GET] News
+    index(req, res) {
+        res.render('news')
+    }
+
+     search(req, res) {
+        res.render('search')
+    }
+
+}
+
+module.exports = new NewsController()
---
+++ src/index.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const route = require('./routes')
+
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))//public file image in file public
+//HTTP logger
+app.use(morgan('combined'))
+
+//Template engine
+app.engine('hbs', engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+route(app)
+
+app.listen(port, () => console.log(`Example app lisnten port ${port}`))
